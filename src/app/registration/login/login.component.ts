b740facaf3c8e7e12176a5f697977e4ae3dfbcc5
mjsuites <mjsuites2020@gmail.com>
2022-12-09 15:41:19 +0500

    restrictZero

--- src/app/registration/login/login.component.ts
+++ src/app/registration/login/login.component.ts
@@ -9,8 +9,26 @@ export class LoginComponent implements OnInit {
   getval: any;
   sum = 2 + 2;
 
+  allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  specialKeys = ['Backspace', 'ArrowRight', 'ArrowLeft'];
+
   ngOnInit(): void {}
   getvalue(_user: number) {
     this.getval = 3 + _user;
+  }
+
+  restrictZero(event: any) {
+    if (!event) return;
+    if (this.specialKeys.includes(event.key)) return;
+
+    if (!this.allowedKeys.includes(event.key)) {
+      event.preventDefault();
+    }
+
+    const value = event.target.value;
+    const newValue = value + event.key;
+    if (Number(newValue) > 100) {
+      event.preventDefault();
+    }
   }
 }
